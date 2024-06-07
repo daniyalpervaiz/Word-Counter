aeb61354d1cb4bb01603f743fa97c38b541c508b
Daniyal Pervaiz <160670546+daniyalpervaiz@users.noreply.github.com>
2024-06-08 02:20:50 +0500

    Add files via upload

---
+++ index.ts
@@ -0,0 +1,16 @@
+#!/usr/bin/env node
+
+import inquirer from "inquirer"
+
+let answer_Sentence=await inquirer.prompt([{
+    message:"Enter The Sentence",
+    type:"input",
+    name:"sentence"
+    
+
+
+}])
+
+let count_Words=answer_Sentence.sentence.trim().split(" ")
+console.log(count_Words)
+console.log(`your Sentence words are ${count_Words.length}`)
